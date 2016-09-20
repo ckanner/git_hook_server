a32c0e4ec0e40944d1a537e132c88ef8f1d266d5
kanner <jiaqing.chen@extantfuture.com>
2016-09-20 22:24:45 +0800

    add log

--- main.go
+++ main.go
@@ -7,12 +7,13 @@ import (
 	"io/ioutil"
 	"github.com/julienschmidt/httprouter"
 	"encoding/json"
+	"log"
 )
 
 func hookHandler(w http.ResponseWriter, r * http.Request, ps httprouter.Params)  {
 	// parse body and params
 	service_name := ps.ByName("service")
-	fmt.Println("service_name=", service_name)
+	log.Println("hookHandler service_name is " + service_name)
 	result, _ := ioutil.ReadAll(r.Body)
 	r.Body.Close()
 	var f interface{}
@@ -20,8 +21,7 @@ func hookHandler(w http.ResponseWriter, r * http.Request, ps httprouter.Params) 
 	data_map := f.(map[string]interface{})
 	// object_kind, ref
 	object_kind := data_map["object_kind"].(string)
-	//ref_branch := data_map["ref"].(string)
-	//fmt.Println("ref=", ref_branch)
+	log.Println("hookHandler object_kind is " + object_kind)
 
 	var env_name = ""
 	if strings.EqualFold(object_kind, "push") {
@@ -31,33 +31,6 @@ func hookHandler(w http.ResponseWriter, r * http.Request, ps httprouter.Params) 
 		} else if strings.EqualFold(ref_branch, "refs/heads/master") {
 			env_name = "formal"
 		}
-		fmt.Println("send job request")
-		// send job request
-		//env_name := "dev"
-		//if strings.EqualFold(ref_branch, "ref/head")
-		//req_url := fmt.Sprintf()
-		//req, err := http.NewRequest("GET", "https://ci.office.extantfuture.com/job/dev_java_common/buildWithParameters?token=af100519383a99866be4bead138c081c&ENV_NAME=dev", nil)
-		//if err != nil {
-		//	fmt.Println(err)
-		//}
-		//req.SetBasicAuth("backend", "af100519383a99866be4bead138c081c")
-		//
-		//resp, err := http.DefaultClient.Do(req)
-		//if err != nil {
-		//	fmt.Println("err err")
-		//}
-		//defer resp.Body.Close()
-
-
-		//u, _ := url.Parse("https://backend:af100519383a99866be4bead138c081c@ci.office.extantfuture.com/job/dev_java_common/buildWithParameters")
-		//query_params := u.Query()
-		//query_params.Set("token", "af100519383a99866be4bead138c081c")
-		//query_params.Set("ENV_NAME", "dev")
-		//u.RawQuery = query_params.Encode()
-		//res, _ := http.Get(u.String())
-		//result, _ := ioutil.ReadAll(res.Body)
-		//res.Body.Close()
-		//fmt.Println(result)
 	} else if strings.EqualFold(object_kind, "merge_request") {
 		object_attributes := data_map["object_attributes"].(map[string]interface{})
 		target_branch := object_attributes["target_branch"].(string)
@@ -70,6 +43,8 @@ func hookHandler(w http.ResponseWriter, r * http.Request, ps httprouter.Params) 
 			}
 		}
 	}
+
+	log.Println("hookHandler env_name is " + env_name)
 	if !strings.EqualFold(env_name, "") {
 		req_url := fmt.Sprintf("https://ci.office.extantfuture.com/job/%s_%s/buildWithParameters?token=af100519383a99866be4bead138c081c&ENV_NAME=%s", env_name, service_name, env_name)
 		req, err := http.NewRequest("GET", req_url, nil)
